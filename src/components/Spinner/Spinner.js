469666878d3ef7cbded1121e96429a361bdea945
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-10-11 09:32:11 +0200

    Change syntax from class to className

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,7 +4,7 @@ import './Spinner.css';
 const Spinner = () => {
     return (
         <>
-            <div class="loadingio-spinner-spinner-dv5orxfimbh"><div class="ldio-x6z8n7cmwg9">
+            <div className="loadingio-spinner-spinner-dv5orxfimbh"><div className="ldio-x6z8n7cmwg9">
             <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
             </div></div>
         </>
