65097c4dcf3b5abbce8634763879a599c86ed672
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-12-16 06:49:24 +0100

    Destructure props

--- src/components/Pages/PokemonDescription/PokemonDescription.js
+++ src/components/Pages/PokemonDescription/PokemonDescription.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { getTypeColor } from '../../../actions/getTypeColor/getTypeColor';
 
-const PokemonDescription = (props) => {
-  console.log(props.location.pokemonProps.stats);
+const PokemonDescription = ({ location: {pokemonProps: { name, img, type, height, weight, stats }} }) => {
   return (
     <div>
       <Link to="/">Go back</Link>
-      <h1>{props.location.pokemonProps.name}</h1>
-      <img src={props.location.pokemonProps.img} alt="pokemon" />
+      <h1>{name}</h1>
+      <img src={img} alt="pokemon" />
       <div>
-          {props.location.pokemonProps.type.map(type => (
+          {type.map(type => (
               <p style={{ backgroundColor: getTypeColor(type.type.name) }}>{type.type.name}</p>
           ))}
       </div>
       <div>
-        <p>{props.location.pokemonProps.height}</p>
-        <p>{props.location.pokemonProps.weight}</p>
+        <p>{height}</p>
+        <p>{weight}</p>
       </div>
-      {props.location.pokemonProps.stats.map(stat => (
+      {stats.map(stat => (
         <div>
           <p>{stat.stat.name}</p>
           <p>{stat.base_stat}</p>
