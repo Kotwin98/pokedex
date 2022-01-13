2ba6810136b09aeb334a064e6e790d04206b5bec
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-01-13 08:10:26 +0100

    Add pokemon evolution chain

--- src/components/Pages/PokemonDescription/PokemonDescription.js
+++ src/components/Pages/PokemonDescription/PokemonDescription.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { 
+  Radar, 
+  RadarChart, 
+  PolarGrid, 
+  PolarAngleAxis, 
+  PolarRadiusAxis, 
+  ResponsiveContainer 
+} from 'recharts';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { getTypeColor } from '../../../actions/getTypeColor/getTypeColor';
 
-const PokemonDescription = ({ location: {pokemonProps: { name, img, type, height, weight, stats }} }) => {
+const PokemonDescription = ({ location: {pokemonProps: { id, name, img, type, height, weight, stats }} }) => {
+  const [firstEvolution, setFirstEvolution] = useState();
+  const [secondEvolution, setSecondEvolution] = useState();
+  const api = `https://pokeapi.co/api/v2/evolution-chain/${id}`
+
+  useEffect(() => {
+    axios.get(api)
+      .then(function (response) {
+        setFirstEvolution(response.data.chain.evolves_to[0].species.name);
+        setSecondEvolution(response.data.chain.evolves_to[0].evolves_to[0].species.name);
+        // console.log(response.data.chain.evolves_to[0].species.name);
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  });
+
   return (
     <div>
       <Link to="/">Go back</Link>
@@ -23,6 +49,9 @@ const PokemonDescription = ({ location: {pokemonProps: { name, img, type, height
           <p>{stat.base_stat}</p>
         </div>
       ))}
+      <div>
+        <p>{name} - {firstEvolution} - {secondEvolution}</p>
+      </div>
     </div>
   );
 }
